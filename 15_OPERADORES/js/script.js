67b032e22e83b5c8ab1065cc606a2f12a6b0b167
Henrique <henriqueflavioguimaraes@gmail.com>
2025-09-24 13:34:37 -0300

    operacoes e tipos

--- 15_OPERADORES/js/script.js
+++ 15_OPERADORES/js/script.js
@@ -7,3 +7,24 @@ console.log(typeof 14.3);
 
 console.log(-5);
 console.log(typeof -5);
+
+// 2 - Ops
+
+console.log("Soma: ", 2 + 2);
+console.log("Subtração: ", 10 - 5);
+console.log("Divisao: " + 4 / 2);
+console.log("Produto: " + 5 * 10);
+console.log("Operacao: ", 2 + (2 * 2));
+
+// 3 - Special Numbers
+
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(12 * "OI");
+console.log(typeof NaN);
+
+// 4 - Strings
+
+console.log("Um texto");
+console.log('Outro texto');
+console.log(`Mais um texto`);
